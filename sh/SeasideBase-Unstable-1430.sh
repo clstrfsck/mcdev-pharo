47aeb37a7aa2cd3875b0892a7938304db7e657d7
Martin Sandiford <ms@mcdev.com.au>
2012-04-06 00:33:34 +0930

    Scripts and assorted configuration files for SeasideBase-Unstable build

--- sh/SeasideBase-Unstable-1430.sh
+++ sh/SeasideBase-Unstable-1430.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
+OUTFILE=SeasideBase-Unstable-1430.zip
+
 mkdir -p package-cache
 
 rm -rf tmp && mkdir tmp
 unzip -qjo -d tmp Pharo-Unstable-1.4.zip
 
-for f in st/before.st st/seasidebase.st st/after.st; do
+for f in st/before.st st/seaside3-base.st st/seaside3-ajp.st st/seaside3-zinc.st st/rfb.st st/after.st; do
     cat "$f" >>tmp/seasidebase.st
     echo "!" >>tmp/seasidebase.st
 done
@@ -17,10 +19,14 @@ if [ -f tmp/crash.dmp ]; then
     echo "Build failed: VM crashed"
     exit 1
 fi
-if [ -f tmp/PharoDebug.log ]; then
+if [ -f tmp/PharoDebug.log ] && grep -q THERE_BE_DRAGONS_HERE tmp/PharoDebug.log; then
     echo "Build failed: Execution failed"
     exit 1
 fi
 
-echo "Seaside 3.0 as loaded in seasidebase.st" >> tmp/VERSION
 rm -rf tmp/package-cache
+rm -f tmp/PharoDebug.log
+echo "Seaside 3.0 as loaded in seasidebase.st" >> tmp/VERSION
+zip -qrj "$OUTFILE" tmp/*
+rm -rf tmp
+echo "$OUTFILE created"
