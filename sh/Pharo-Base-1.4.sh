7538cc8e47e5deb847937d9056769b70a9e0c6ca
Martin Sandiford <ms@mcdev.com.au>
2012-04-06 16:00:56 +0930

    Added Pharo-Base-1.4 build config with tests.

---
+++ sh/Pharo-Base-1.4.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# Bootstrap Pharo 1.4 Linux VM and image from upstream
+set -e
+
+# Some useful vars for later
+SCRIPTDIR="`dirname $0`"
+PKGNAME="`basename $0 .sh`"
+
+# Output and test results we will generate
+OUTFILE="${PKGNAME}.zip"
+TSTFILE="${PKGNAME}-Tests.zip"
+
+# Upstream Jenkins
+UPSTREAM=https://ci.lille.inria.fr/pharo/job
+
+# VM, image and sources we need to bootstrap
+VMPROJ="Cog-Unix"
+VM_ART=Cog.zip
+PHPROJ="Pharo%201.4"
+PH_ART=Pharo-1.4.zip
+SRCURL=https://gforge.inria.fr/frs/download.php/24391/PharoV10.sources.zip
+SRCFILE=PharoV10.sources.zip
+
+# Get the last successful build number from upstream Jenkins
+# First argument is job name
+function get_buildnum {
+    # Delete everything but numbers using tr to avoid malicious input
+    curl -fsS "$UPSTREAM/$1/lastSuccessfulBuild/buildNumber" | tr -dc 0123456789
+}
+
+# Retrieve an upstream artifact into a local file
+# First argument is local output file
+# Second is Jenkins job name
+# Third is build number
+# Fourth is Jenkins artifact name
+function get_buildfile {
+    if [ ! -e "$1" ]; then
+        echo "Fetching $2/$3/$4 -> $1"
+	rm -f "$1.tmp"
+        curl -fsS -o "$1.tmp" "$UPSTREAM/$2/$3/artifact/$4"
+        mv -f "$1.tmp" "$1"
+    else
+	echo "$1 is up to date"
+    fi
+}
+
+# Retrieve VM into local zip file from Jenkins
+# Local file includes build number for tracking
+echo "Checking current $VMPROJ build number"
+VBLD=`get_buildnum "$VMPROJ"`
+VMFILE=cog-linux-${VBLD}.zip
+get_buildfile "$VMFILE" "$VMPROJ" ${VBLD} "$VM_ART"
+
+# Retrieve Pharo 1.4 image into local zip file from Jenkins
+# Local file includes build number and update number for tracking
+# We can use the update number here as a regular convention is used
+echo "Checking current $PHPROJ build number"
+PBLD=`get_buildnum "$PHPROJ"`
+PURL="$UPSTREAM/$PHPROJ/$PBLD/api/xml?xpath=/*/description"
+PREV=`curl -fsS "${PURL}" | cut -f2 -d " " | tr -dc 0123456789`
+echo "Build number $PBLD revision $PREV"
+PHFILE="Pharo-1.4-${PBLD}-${PREV}.zip"
+get_buildfile "$PHFILE" "$PHPROJ" ${PBLD} "$PH_ART"
+
+# Using a new build directory, unpack the upstream bits.
+# We then annotate the output with the versions of the VM
+# and image that are included.
+rm -rf build && mkdir build
+unzip -qjo -d build "$PHFILE"
+unzip -qjo -d build "$VMFILE"
+echo "Cog VM build $VBLD from $UPSTREAM/$VMPROJ" >> build/VERSIONS
+echo "Pharo 1.4 image build $PBLD rev $PREV from $UPSTREAM/$PHPROJ" >> build/VERSIONS
+zip -qrj "$OUTFILE" build/*
+echo "$OUTFILE created"
+
+# Run the tests and zip these up into an artifact as well.
+"$SCRIPTDIR/runscripts.sh" "$PKGNAME" Pharo-1.4.image st/buildtools.st st/pharo14-runtests.st
+zip -qrj "$TSTFILE" build/*
+echo "$TSTFILE created"
+
+## END ##
---
+++ sh/runscripts.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Must supply a package name as the first argument"
+    exit 1
+fi
+PKGNAME="$1"
+
+if [ -z "$2" ]; then
+    echo "Must supply an image name as the second argment"
+    exit 1
+fi
+IMGNAME="$2"
+
+if [ -z "$3" ]; then
+    echo "Must supply one or more Smalltalk scripts to run"
+    exit 1
+fi
+shift 2
+SCRIPTS="$*"
+
+for f in st/before.st $SCRIPTS st/after.st; do
+    cat "$f" >>"build/${PKGNAME}.st"
+    echo "!" >>"build/${PKGNAME}.st"
+done
+
+echo "Building package $PKGNAME"
+(cd build; ln -s ../package-cache; ./CogVM -nodisplay -nosound "${IMGNAME}" "${PKGNAME}.st"; rm -f package-cache)
+
+if [ -f build/crash.dmp ]; then
+    echo "Build failed: VM crashed"
+    exit 1
+fi
+if [ -f build/PharoDebug.log ] && grep -q THERE_BE_DRAGONS_HERE build/PharoDebug.log; then
+    echo "Build failed: Execution failed; check build/PharoDebug.log"
+    exit 1
+fi
