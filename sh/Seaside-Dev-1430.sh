c11798850bec12b31f7811c3f1be4c19ea8e52e5
Martin Sandiford <ms@mcdev.com.au>
2012-04-07 02:10:47 +0930

    Added Seaside-Dev-1430 build scripts.

---
+++ sh/Seaside-Dev-1430.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Bootstrap Seaside 3.0 basics
+set -e
+
+# Some useful vars for later
+SCRIPTDIR="`dirname $0`"
+PKGNAME="`basename $0 .sh`"
+TSTNAME="${PKGNAME}-Tests"
+IMGNAME=Pharo-1.4.image
+PKGPARENT=Seaside-Base-1430-Tests
+
+# Output and test results we will generate
+OUTFILE="${PKGNAME}.zip"
+TSTFILE="${TSTNAME}.zip"
+
+# Using a new build directory, unpack the upstream bits.
+rm -rf build && mkdir build
+unzip -qjo -d build "${PKGPARENT}.zip"
+rm -f build/*-Test.xml
+echo "${PKGNAME} as loaded by ${PKGNAME}.st" >> build/VERSIONS
+
+# Build the base image
+"$SCRIPTDIR/runscripts.sh" "${PKGNAME}" "$IMGNAME" st/omnibrowser.st st/seaside3-dev.st
+zip -qrj "$OUTFILE" build/*
+echo "$OUTFILE created"
+
+# Build the test image
+echo "${TSTNAME} as loaded by ${TSTNAME}.st" >> build/VERSIONS
+"$SCRIPTDIR/runscripts.sh" "${TSTNAME}" "$IMGNAME" \
+    st/buildtools.st \
+    st/seaside3-dev-tests.st \
+    st/omnibrowser-tests.st \
+    st/pharo14-runtests.st \
+    st/seaside3-runtests.st \
+    st/omnibrowser-runtests.st
+zip -qrj "$TSTFILE" build/*
+echo "$TSTFILE created"
